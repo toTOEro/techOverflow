984792d9ecc67cb725b9a13edb2774768c271bb4
hdela <hfdelacr@ucsd.edu>
2022-11-16 20:10:47 -0800

    still modifying Home and Profile pages but no info yet.

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import {ApolloClient, InMemoryCache, ApolloProvider } from `@apollo/client`;
+
+import {BrowserRouter as Router, Routes, Route} from `react-router-dom`;
+
+
+const client = new ApolloClient({
+  uri: '/graphql',
+  cache: new InMemoryCache(),
+})
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ <ApolloProvider client={client}>
+  <Router>
+    {/*TBD*/}
+    <div className= "flex-column"  >
+      <Header/>
+      <div className="container">
+        <Routes> 
+        <Route 
+        path="/"
+        element={<Home/>}
+        />
+        <Route
+        path="/profiles/:profileID"
+        element={<Profile/>}
+        />
+      </Routes>
     </div>
+    <Footer/>
+    </div>
+
+  </Router>
+ </ApolloProvider>
   );
 }
 
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,3 +3,35 @@ import { useQuery } from '@apollo/client';
 
 import PostingList from `../components/PostList`;
 import PostingForm from `../components/PostingForm`;
+
+import {QUERY_POSTINGS} from `../utils/queries`;
+
+const Home = () => {
+    const {loading, data} = useQuery(QUERY_POSTINGS);
+    const postings = data?.postings || [];
+
+    return (
+        <main> 
+            <div className="flex-row justify-center">
+                <div 
+                className="col-12 col-md-10 mb-3 p-3" 
+                style={{ border: `1px dotted #1a1a1a` }}
+                >
+                    <PostingForm />
+                </div>
+
+                <div className="col-12">
+                    {loading ? (
+                        <div>Loading...</div>
+                    ): (
+                        <PostingList 
+                        postings={postings}
+                        title="Here's the current list of postings"
+                        />
+                    )}
+                </div>
+            </div>
+        </main>
+    );
+};
+export default Home;
