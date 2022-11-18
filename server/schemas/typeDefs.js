708697e3df52b4582c71871f810f85fec5b785bd
MarioArmstrong <arms.mario@icloud.com>
2022-11-17 20:12:24 -0800

    fixing some typeDefs and adding mutations to typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,45 +1,52 @@
 const { gql } = require("apollo-server-express");
 const typeDefs = gql`
-
-type User {
+  type User {
     _id: ID
     firstName: String!
     lastName: String!
     email: String!
-    password String!
+    password: String!
     postings: [Posting]
-}
+  }
 
-type Posting {
+  type Posting {
     _id: ID
     title: String!
     description: String
     date_created: String
     comments: [Comment]
-}
+  }
 
-type Comment {
+  type Comment {
     _id: ID
     content: String!
     date_created: String
-}
+  }
 
-type Auth {
+  type Auth {
     token: ID
     user: User
-}
+  }
 
-type Query {
+  type Query {
     user: User
-    posting(_id:ID): Posting
+    posting(_id: ID): Posting
     comment: [Comment]
-}
+  }
 
-type Mutation {
-    addUser(firstName: String!, lastName: String!, email: String!, password: String!): Auth
-    addPosting(title: String!, description: String, date_created: String, comments: ID):
-    addComment():
-    login(email: String!, password: String!): Auth}
+  type Mutation {
+    addUser(
+      firstName: String!
+      lastName: String!
+      email: String!
+      password: String!
+    ): Auth
+    addPosting(title: String!, description: String): Posting
+    addComment(content: String!): Comment
+    deletePosting(_id: ID!): Posting
+    deleteComment(_id: ID!): Comment
+    login(email: String!, password: String!): Auth
+  }
 `;
 
 module.exports = typeDefs;
