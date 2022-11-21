1f6e3af6c0e7ec2bf9b5c51f5b5760c001fc1119
MarioArmstrong <arms.mario@icloud.com>
2022-11-20 20:22:15 -0800

    resolvers done, but without authentication

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -42,61 +42,44 @@ const resolvers = {
 
       return { token, user };
     },
-    updateUser: async (parent, args, context) => {
-      // if (context.user) {
-      return await User.findByIdAndUpdate(context.user._id, args, {
-        new: true,
-      });
-      // }
-
-      // throw new AuthenticationError("Error: unable to do that!");
+    updateUser: async (
+      parent,
+      { _id, firstName, lastName, email, password },
+      context
+    ) => {
+      return User.findByIdAndUpdate(
+        _id,
+        { firstName, lastName, email, password },
+        {
+          new: true,
+        }
+      );
     },
 
-    deleteUser: async (parent, args, context) => {
-      // if (context.user) {
-      return User.findOneAndDelete({ _id: context.user._id });
-      // }
-      // throw new AuthenticationError("Error: unable to do that!");
+    deleteUser: async (parent, { _id }, context) => {
+      return User.findByIdAndDelete({ _id });
     },
-
     addPosting: async (parent, args, context) => {
-      // if (context.user) {
       return Posting.create(args);
-      // }
-      // throw new AuthenticationError("You need to be logged in to make a post!");
     },
     updatePosting: async (parent, { _id, title, description }, context) => {
-      // if (context.user) {
       return Posting.findByIdAndUpdate(
         _id,
         { title, description },
         { new: true }
       );
-      // }
-      // throw new AuthenticationError("You cannot do that!");
     },
     deletePosting: async (parent, { _id }, context) => {
-      // if (context.user) {
       return Posting.findByIdAndDelete({ _id });
-      // }
-      // throw new AuthenticationError("You can't do that!");
     },
     addComment: async (parent, args, context) => {
-      // if (context.user) {
       return Comment.create(args);
-      // }
-      // throw new AuthenticationError("You need to be logged in to comment!");
     },
     updateComment: async (parent, { _id, content }, context) => {
-      // if (context.user) {
       return Comment.findByIdAndUpdate(_id, { content }, { new: true });
-      // }
     },
     deleteComment: async (parent, { _id }, context) => {
-      // if (context.user) {
       return Comment.findByIdAndDelete({ _id });
-      // }
-      // throw new AuthenticationError("You can't do that!");
     },
   },
 };
