dfb5c2cb0fa0287a9fceea92000431a594a5929c
anthony3738-miZJgd <anthony3738@gmail.com>
2022-11-21 21:26:45 -0800

    Working on posting submission

--- client/src/components/IdeaForm/index.js
+++ client/src/components/IdeaForm/index.js
@@ -1,29 +1,90 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import {
+    Button,
     FormControl,
     FormLabel,
     FormErrorMessage,
     FormHelperText,
     Input,
-    Textarea
+    Modal,
+    ModalBody,
+    ModalCloseButton,
+    ModalContent,
+    ModalFooter,
+    ModalHeader,
+    ModalOverlay,
+    Textarea,
+    useDisclosure
 } from '@chakra-ui/react'
+
+import { FcIdea } from 'react-icons/fc'
+
+import { usePostingContext } from "../../utils/GlobalState"
+
+import { ADD_POSTING } from '../../utils/actions';
+
 
 
 export default function IdeaForm() {
+    const { isOpen, onOpen, onClose } = useDisclosure();
+
+    const [state, dispatch] = usePostingContext();
+    const [newPostTitle, setNewPostTitle] = useState('');
+    const [newPostDescription, setNewPostDescription] = useState('');
+
 
 
     return (
-        <FormControl>
-            <FormLabel>Title</FormLabel>
-            <Input type='text'></Input>
-            <FormLabel>Description</FormLabel>
-            <Textarea 
-                placeholder='Tell us about your idea!'
-                size='lg'
-            />
+        <>
+            <Button onClick={onOpen} colorScheme='whatsapp' rightIcon={<FcIdea />} leftIcon={<FcIdea />}>Submit an Idea!</Button>
 
-        </FormControl>
+            <Modal isOpen={isOpen} onClose={onClose} size='4xl' isCentered='true'>
+                <ModalOverlay />
+                <ModalContent>
+                    <ModalHeader>A new project awaits...</ModalHeader>
+                    <ModalCloseButton />
+                    <ModalBody>
+                        <FormControl>
+                            <FormLabel>Title</FormLabel>
+                            <Input
+                                type='text'
+                                value={newPostTitle}
+                                onChange={(e) => setNewPostTitle(e.target.value)}
+
+                            />
+                            <FormLabel>Description</FormLabel>
+                            <Textarea
+                                placeholder='Tell us about your idea!'
+                                size='lg'
+                                value={newPostDescription}
+                                onChange={(e) => setNewPostDescription(e.target.value)}
+                            />
+                        </FormControl>
+                    </ModalBody>
+
+                    <ModalFooter>
+                        <Button colorScheme='blue' mr={3} onClick={onClose}>
+                            Close
+                        </Button>
+                        <Button colorScheme='green' onClick={() => {
+                            console.log(`Dispatched Title: ${newPostTitle} description: ${newPostDescription}`)
+                          return dispatch({
+                            type: ADD_POSTING,
+                            posting: {
+                                title: newPostTitle,
+                                description: newPostDescription
+                            }
+                          })  
+                        }
+                            
+                        }>Submit</Button>
+                    </ModalFooter>
+
+                </ModalContent>
+
+            </Modal>
+        </>
     )
 
 };
--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,32 +1,19 @@
 import React from 'react';
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import {
     Box,
     HStack,
-    Modal,
-    ModalOverlay,
-    ModalContent,
-    ModalHeader,
-    ModalFooter,
-    ModalBody,
-    ModalCloseButton,
-    useDisclosure,
+
     Button,
     Icon,
-    FormControl,
-    FormLabel,
-    Input,
-    Textarea
+   
 } from '@chakra-ui/react'
 
-import { FaDonate } from 'react-icons/fa'
-import { FcIdea } from 'react-icons/fc'
 import Donate from '../Donate'
 
 import IdeaForm from '../IdeaForm';
 
 export default function Nav() {
-    const { isOpen, onOpen, onClose } = useDisclosure();
 
     return (
         <>
@@ -34,47 +21,122 @@ export default function Nav() {
                 <HStack justifyContent='space-between'>
                     <Button as='a' href='/'><Icon src='./icons8-engineering-64.png'></Icon>LOGO HERE </Button>
 
+                    <IdeaForm />
 
-                    <Button onClick={onOpen} colorScheme='whatsapp' rightIcon={<FcIdea />} leftIcon={<FcIdea />}>Submit an Idea!</Button>
-                    <Button 
-                    as="a"
-                    href="/Signup"
-                    target="_blank"
-                    colorSchema='blue'>Signup</Button>
-                    <Button 
-                    as="a"
-                    href="/Login"
-                    target="_blank"
-                    colorSchema='blue'>Login</Button>
+                    <Button
+                        as="a"
+                        href="/Signup"
+                        target="_blank"
+                        colorScheme='blue'>Signup</Button>
+                    <Button
+                        as="a"
+                        href="/Login"
+                        target="_blank"
+                        colorScheme='blue'>Login</Button>
                     <Donate />
 
                 </HStack>
             </Box>
 
-            <Modal isOpen={isOpen} onClose={onClose} size='4xl' isCentered='true'>
+            {/* <Modal isOpen={isOpen} onClose={onClose} size='4x1' isCentered='true'>
                 <ModalOverlay />
-                <ModalContent>
-                    <ModalHeader>A new project awaits...</ModalHeader>
-                    <ModalCloseButton />
-                    <ModalBody>
-                        <IdeaForm />
-                    </ModalBody>
-
-                    <ModalFooter>
-                        <Button colorScheme='blue' mr={3} onClick={onClose}>
-                            Close
-                        </Button>
-                        <Button colorScheme='green'>Submit</Button>
-                    </ModalFooter>
-
-                </ModalContent>
-
-            </Modal>
-            <Modal isOpen={isOpen} onClose={onClose} size='4x1' isCentered='true'>
-        <ModalOverlay/>
-    </Modal>
+            </Modal> */}
         </>
 
     )
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from 'react';
+// import { Link } from 'react-router-dom'
+// import {
+//     Box,
+//     HStack,
+//     Modal,
+//     ModalOverlay,
+//     ModalContent,
+//     ModalHeader,
+//     ModalFooter,
+//     ModalBody,
+//     ModalCloseButton,
+//     useDisclosure,
+//     Button,
+//     Icon,
+//     FormControl,
+//     FormLabel,
+//     Input,
+//     Textarea
+// } from '@chakra-ui/react'
+
+// import { FaDonate } from 'react-icons/fa'
+// import { FcIdea } from 'react-icons/fc'
+// import Donate from '../Donate'
+
+// import IdeaForm from '../IdeaForm';
+
+// export default function Nav() {
+//     const { isOpen, onOpen, onClose } = useDisclosure();
+
+//     return (
+//         <>
+//             <Box width='100%' bg='black' py='4' marginBottom='20px' px='10px'>
+//                 <HStack justifyContent='space-between'>
+//                     <Button as='a' href='/'><Icon src='./icons8-engineering-64.png'></Icon>LOGO HERE </Button>
+
+                    
+//                     <Button onClick={onOpen} colorScheme='whatsapp' rightIcon={<FcIdea />} leftIcon={<FcIdea />}>Submit an Idea!</Button>
+//                     <Button
+//                         as="a"
+//                         href="/Signup"
+//                         target="_blank"
+//                         colorScheme='blue'>Signup</Button>
+//                     <Button
+//                         as="a"
+//                         href="/Login"
+//                         target="_blank"
+//                         colorScheme='blue'>Login</Button>
+//                     <Donate />
+
+//                 </HStack>
+//             </Box>
+
+//             <Modal isOpen={isOpen} onClose={onClose} size='4xl' isCentered='true'>
+//                 <ModalOverlay />
+//                 <ModalContent>
+//                     <ModalHeader>A new project awaits...</ModalHeader>
+//                     <ModalCloseButton />
+//                     <ModalBody>
+//                         <IdeaForm />
+//                     </ModalBody>
+
+//                     <ModalFooter>
+//                         <Button colorScheme='blue' mr={3} onClick={onClose}>
+//                             Close
+//                         </Button>
+//                         <Button colorScheme='green'>Submit</Button>
+//                     </ModalFooter>
+
+//                 </ModalContent>
+
+//             </Modal>
+//             {/* <Modal isOpen={isOpen} onClose={onClose} size='4x1' isCentered='true'>
+//                 <ModalOverlay />
+//             </Modal> */}
+//         </>
+
+//     )
+
+// }
