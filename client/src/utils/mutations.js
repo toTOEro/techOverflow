f569bfc64955bc1e4f9616a810a8a701e5aef52a
Margot Cooper Gruen <margotgruen@gmail.com>
2022-11-22 20:35:27 -0800

    UPDATE USER mutation

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,18 +1,23 @@
-import { gql } from '@apollo/client';
+import { gql } from "@apollo/client";
 
 export const ADD_USER = gql`
-mutation addUser($firstName: String!, $lastName: String!, $email: String!, $password: String!){
-    addUser(name: $name, email: $email, password: $password){
-        _id
-        firstName
-        lastName
-        email
-        postings
+  mutation addUser(
+    $firstName: String!
+    $lastName: String!
+    $email: String!
+    $password: String!
+  ) {
+    addUser(name: $name, email: $email, password: $password) {
+      _id
+      firstName
+      lastName
+      email
+      postings
     }
-}
+  }
 `;
 
-export const ADD_POSTING = gql `
+export const ADD_POSTING = gql`
 mutation addPosting($userId: ID!, $posting.title: String!, $posting.description: String! ){
     addPosting(userId: $userId, posting.title: $posting.title, posting.description: $posting.description){
         _id
@@ -22,22 +27,26 @@ mutation addPosting($userId: ID!, $posting.title: String!, $posting.description:
     }
 }
 
-`
-
-export const ADD_COMMENT = gql `
-    
-
-
 `;
 
 export const LOGIN_USER = gql`
-mutation login($email: String!, $password: String!){
-    login(email: $email, password: $password){
-        profile {
-            _id
-            firstName
-            lastName
-        }
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      profile {
+        _id
+        firstName
+        lastName
+      }
     }
-}
-`
+  }
+`;
+
+export const UPDATE_USER = gql`
+updateUser($id: ID!, $firstName: String, $lastName: String, $email: String, $password: String) {
+    updateUser(_id: $id, firstName: $firstName, lastName: $lastName, email: $email, password: $password) {
+      lastName
+      firstName
+      email
+    }
+  }
+`;
