86e294303da7c085759629b0b8e492254e64c50d
MarioArmstrong <arms.mario@icloud.com>
2022-11-26 16:19:18 -0800

    pushing drawer that doesn't work perfectly

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,140 +1,69 @@
-import React from 'react';
-import { Link } from 'react-router-dom'
+import React from "react";
+// import { Link } from "react-router-dom";
 import {
-    Box,
-    HStack,
+  Box,
+  Button,
+  Flex,
+  Icon,
+  IconButton,
+  useDisclosure,
+  Drawer,
+  DrawerBody,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+} from "@chakra-ui/react";
+import { FaUser, FaEdit } from "react-icons/fa";
+import { AiTwotoneHome } from "react-icons/ai";
+import { HamburgerIcon } from "@chakra-ui/icons";
 
-    Button,
-    Icon,
-   
-} from '@chakra-ui/react'
+import Donate from "../Donate";
 
-import Donate from '../Donate'
-
-import IdeaForm from '../IdeaForm';
+import IdeaForm from "../IdeaForm";
 
 export default function Nav() {
-
-    return (
-        <>
-            <Box width='100%' bg='black' py='4' marginBottom='20px' px='10px'>
-                <HStack justifyContent='space-between'>
-                    <Button as='a' href='/'><Icon src='./icons8-engineering-64.png'></Icon>LOGO HERE </Button>
-
-                    <IdeaForm />
-
-                    <Button
-                        as="a"
-                        href="/Signup"
-                        target="_blank"
-                        colorScheme='blue'>Signup</Button>
-                    <Button
-                        as="a"
-                        href="/Login"
-                        target="_blank"
-                        colorScheme='blue'>Login</Button>
-                    <Donate />
-
-                </HStack>
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  return (
+    <>
+      {/* <IconButton onClick={onOpen} icon={<HamburgerIcon />} /> */}
+      <Button onClick={onOpen}>
+        <HamburgerIcon />
+        Navigation
+      </Button>
+      <Drawer isOpen={isOpen} placement="left" onClose={onClose} size="xs">
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton />
+          <DrawerBody>
+            <Box>
+              <Button as="a" href="/">
+                <AiTwotoneHome />
+                Home
+              </Button>
             </Box>
-
-
-        </>
-
-    )
-
+            <Box>
+              <IdeaForm />
+            </Box>
+            <Box>
+              <Button as="a" href="/Signup" colorScheme="blue">
+                <FaEdit />
+                Signup
+              </Button>
+            </Box>
+            <Box>
+              <Button as="a" href="/Login" colorScheme="blue">
+                <FaUser />
+                Login
+              </Button>
+            </Box>
+          </DrawerBody>
+          <DrawerFooter>
+            <Donate />
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+    </>
+  );
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import React from 'react';
-// import { Link } from 'react-router-dom'
-// import {
-//     Box,
-//     HStack,
-//     Modal,
-//     ModalOverlay,
-//     ModalContent,
-//     ModalHeader,
-//     ModalFooter,
-//     ModalBody,
-//     ModalCloseButton,
-//     useDisclosure,
-//     Button,
-//     Icon,
-//     FormControl,
-//     FormLabel,
-//     Input,
-//     Textarea
-// } from '@chakra-ui/react'
-
-// import { FaDonate } from 'react-icons/fa'
-// import { FcIdea } from 'react-icons/fc'
-// import Donate from '../Donate'
-
-// import IdeaForm from '../IdeaForm';
-
-// export default function Nav() {
-//     const { isOpen, onOpen, onClose } = useDisclosure();
-
-//     return (
-//         <>
-//             <Box width='100%' bg='black' py='4' marginBottom='20px' px='10px'>
-//                 <HStack justifyContent='space-between'>
-//                     <Button as='a' href='/'><Icon src='./icons8-engineering-64.png'></Icon>LOGO HERE </Button>
-
-                    
-//                     <Button onClick={onOpen} colorScheme='whatsapp' rightIcon={<FcIdea />} leftIcon={<FcIdea />}>Submit an Idea!</Button>
-//                     <Button
-//                         as="a"
-//                         href="/Signup"
-//                         target="_blank"
-//                         colorScheme='blue'>Signup</Button>
-//                     <Button
-//                         as="a"
-//                         href="/Login"
-//                         target="_blank"
-//                         colorScheme='blue'>Login</Button>
-//                     <Donate />
-
-//                 </HStack>
-//             </Box>
-
-//             <Modal isOpen={isOpen} onClose={onClose} size='4xl' isCentered='true'>
-//                 <ModalOverlay />
-//                 <ModalContent>
-//                     <ModalHeader>A new project awaits...</ModalHeader>
-//                     <ModalCloseButton />
-//                     <ModalBody>
-//                         <IdeaForm />
-//                     </ModalBody>
-
-//                     <ModalFooter>
-//                         <Button colorScheme='blue' mr={3} onClick={onClose}>
-//                             Close
-//                         </Button>
-//                         <Button colorScheme='green'>Submit</Button>
-//                     </ModalFooter>
-
-//                 </ModalContent>
-
-//             </Modal>
-//             {/* <Modal isOpen={isOpen} onClose={onClose} size='4x1' isCentered='true'>
-//                 <ModalOverlay />
-//             </Modal> */}
-//         </>
-
-//     )
-
-// }
