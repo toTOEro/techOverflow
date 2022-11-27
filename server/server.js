d5edb7028e203f971c9b2fb0e372e3250cf5cb1c
anthony3738-miZJgd <anthony3738@gmail.com>
2022-11-27 02:20:04 -0800

    Added app.use

--- server/server.js
+++ server/server.js
@@ -21,6 +21,7 @@ app.use(express.json());
 app.use("/images", express.static(path.join(__dirname, "../client/images")));
 
 if (process.env.NODE_ENV === "production") {
+  app.use(express.static(__dirname));
   app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
